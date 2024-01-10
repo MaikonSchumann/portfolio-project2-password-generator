72b96d45b51af452f33dd84bbbc674ab776f7a2b
MaikonSchumann <71983707+MaikonSchumann@users.noreply.github.com>
2024-01-10 16:06:23 +0000

    feat(script): add functions to get random letters, numbers and symbols

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,27 @@ const lettersInput = document.querySelector("letters");
 const numbersInput = document.querySelector("numbers")
 const symbolsInput = document.querySelector("symbols");
 
+// Get random lower case letters
+function getLetterLowerCase(){
+    const lowerCase = "abcdefghijklmnopqrstuvxz";
+    return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    console.log(lowerCase);
+};
 
+// Get random upper case letters
+function getLetterUpperCase() {
+    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVXZ";
+    return upperCase[Math.floor(Math.random() * upperCase.length)]
+};
 
+// gent random numbers
+function getNumbers() {
+    const numbers = "0123456789";
+    return numbers[Math.floor(Math.random() * numbers.length)]
+};
+
+// Get random symbols
+function getSymbols() {
+    const symbols = "!@#$%&*(){}[]";
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
