3b69591cf5fd91c196f65849c11e58068b7ddc99
MaikonSchumann <71983707+MaikonSchumann@users.noreply.github.com>
2024-01-10 15:17:53 +0000

    feat(script): add js folder/js file and set the javascript elements

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Elements
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("password");
+
+// Inputs
+const lengthInput = document.querySelector("#length");
+const lettersInput = document.querySelector("letters");
+const numbersInput = document.querySelector("numbers")
+const symbolsInput = document.querySelector("symbols");
+
+
+
