82341b2994b6d21d9a2cfbaaf18589d8ce9d8200
MaikonSchumann <71983707+MaikonSchumann@users.noreply.github.com>
2024-01-19 19:45:57 +0000

    fix(script): refactor javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,62 @@
 // Elements
 const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordElement = document.querySelector("#generated-password");
-const copyPasswordButton = document.querySelector("#copy-password");
 
-// Inputs
 const lengthInput = document.querySelector("#length");
 const lettersInput = document.querySelector("#letters");
 const numbersInput = document.querySelector("#numbers");
 const symbolsInput = document.querySelector("#symbols");
+const copyPasswordButton = document.querySelector("#copy-password");
 
-// Get random lower case letters
-function getLetterLowerCase() {
-    const lowerCase = "abcdefghijklmnopqrstuvxz";
-    return lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    console.log(lowerCase);
-}
+const lowerCase = "abcdefghijklmnopqrstuvxz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVXZ";
+const numbers = "0123456789";
+const symbols = "!@#$%&*(){}[]";
 
-// Get random upper case letters
-function getLetterUpperCase() {
-    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVXZ";
-    return upperCase[Math.floor(Math.random() * upperCase.length)];
-}
-
-// gent random numbers
-function getNumber() {
-    const numbers = "0123456789";
-    return numbers[Math.floor(Math.random() * numbers.length)];
-}
-
-// Get random symbols
-function getSymbol() {
-    const symbols = "!@#$%&*(){}[]";
-    return symbols[Math.floor(Math.random() * symbols.length)];
+// Functions
+// Function to generate a random character
+function getCharacter(character) {
+    return character[Math.floor(Math.random() * character.length)];
 }
 
 // Generate password function
-function generatePassword(
-    getLetterLowerCase,
-    getLetterUpperCase,
-    getNumber,
-    getSymbol
-) {
-    // Set a password to a empty string
+function generatePassword() {
     let password = "";
-
-    // Get the user choice for length from the length input
     const passwordLength = +lengthInput.value;
-
-    // Set a generator to receiving the push if checkbox input is checked
     const generators = [];
 
-    // Checking the letters checkbox and pushing a random letter to the generators array
-    if (lettersInput.checked) {
-        generators.push(getLetterLowerCase, getLetterUpperCase);
-    }
+    while (generators.length <= passwordLength) {
+        // Checking letters checkbox and add to generators array
+        if (lettersInput.checked) {
+            for ( i = 0; i < passwordLength; i++){
+                generators.push(getCharacter(lowerCase), getCharacter(upperCase));
+            };
+        };
+        // Checking numbers checkbox and add to generators array
+        if (numbersInput.checked) {
+            for ( i = 0; i < passwordLength; i++){
+                generators.push(getCharacter(numbers));
+            };
+        };
+        // Checking symbols checkbox and add to generators array
+        if (symbolsInput.checked) {
+            for ( i = 0; i < passwordLength; i++){
+                generators.push(getCharacter(symbols));
+            };
+        };
+    };
 
-    // Checking the number checkbox and pushing a random number to the generators array
-    if (numbersInput.checked) {
-        generators.push(getNumber);
-    }
-
-    // Checking the symbol checkbox and pushing a random symbol to the generators array
-    if (symbolsInput.checked) {
-        generators.push(getSymbol);
-    }
-
-    // Checking if NO checkbox is checked and showing an alert to the user
-    if (generators.length === 0) {
-        alert("No caracteres selected! Select your options and generate again!");
-        generatedPasswordElement.querySelector("#password").innerText = "Select your options and generate again!";
-        return;
-    }
-
-    // Making a loop to set the password length as user choice
     for (let i = 0; i < passwordLength; i++) {
-        password += generators[Math.floor(Math.random() * generators.length)]();
-    }
+      password += generators[Math.floor(Math.random() * generators.length)];
+    };
 
-    // Sending the password value to the generatedPasswordElement to be shown to user
-    generatedPasswordElement.querySelector("#password").innerText = password;
-}
+    generatedPasswordElement.querySelector("#password").innerText = password;  
+};
 
 // Events
 // Call generatePassword function when the button is clicked
 generatePasswordButton.addEventListener("click", () => {
-    generatePassword(
-        getLetterLowerCase,
-        getLetterUpperCase,
-        getNumber,
-        getSymbol
-    );
+    generatePassword();
 });
 
 // Password event listener
